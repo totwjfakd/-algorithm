4f27e29b294a6c1bc61860aa51cc7e6fc48b777c
totwjfakd <totwjfakd@gmail.com>
2024-02-28 02:03:26 +0900

    2024.02.28

--- 연습장/aa.py
+++ 연습장/aa.py
@@ -1,18 +1,25 @@
 import sys
 
-input = sys.stdin.readline
-while True :
-    n, m = map(int, input().split())
-    if n == 0 and m == 0 :
-        break
-    dict_ = {}
+def calculate_min_cost(n, w, current_path, visited, current_cost):
+    global min_cost
+    if len(current_path) == n and w[current_path[-1]][current_path[0]] > 0:
+        total_cost = current_cost + w[current_path[-1]][current_path[0]]
+        min_cost = min(min_cost, total_cost)
+        return
+    
+    for next_city in range(n):
+        if not visited[next_city] and w[current_path[-1]][next_city] > 0:
+            visited[next_city] = True
+            calculate_min_cost(n, w, current_path + [next_city], visited, current_cost + w[current_path[-1]][next_city])
+            visited[next_city] = False
 
-    for i in range(n) :
-        dict_[int(input())] = 1
-    count = 0 
-    for i in range(m) :
-        num = int(input())
-        if num in dict_ : 
-            count += 1
+n = int(input())
+w = [list(map(int, input().split())) for _ in range(n)]
 
-    print(count)
+min_cost = sys.maxsize
+for i in range(n):
+    visit = [False for _ in range(n)]
+    visit[i] = True
+    calculate_min_cost(n, w, [i], visit, 0)
+
+print(min_cost)
