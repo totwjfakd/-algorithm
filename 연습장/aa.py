bd2f707a6a3dd99d96389ff9931b544786adc5cc
totwjfakd <totwjfakd@gmail.com>
2024-02-29 01:08:59 +0900

    2024.02.29

--- 연습장/aa.py
+++ 연습장/aa.py
@@ -1,26 +1,54 @@
 import sys
+from collections import deque
 
-def dfs(depth, choice, visit, start) :
-    global count
-    if depth == N+1 :
-        return
-    if depth >= 2 :
-        if L <= sum(choice) <= R and max(choice) - min(choice) >= X:
-            count += 1
-    for i in range(start, N) :
-        if not visit[i] :
-            visit[i] = True
-            dfs(depth+1, choice + [A[i]], visit, i)
-            visit[i] = False
-count = 0
+def make_combination(select_count, select_xy, visit, start) :
+    global combination
+    if select_count == m :
+        combination.append(select_xy)
+    
+    for i in range(start, len(chicken_xy)) :
+        if visit[i] :
+            continue
+        visit[i] = True
+        make_combination(select_count+1, select_xy+[chicken_xy[i]], visit, i+1)
+        visit[i] = False
 
-N, L, R, X = map(int, input().split())
 
-A = list(map(int, input().split()))
 
-for i in range(N) :
-    visit = [False for _ in range(N)]
+input = sys.stdin.readline
 
-    visit[i] = True
-    dfs(1,[A[i]], visit, i)
-print(count)
+n, m = map(int, input().split())
+
+map_ = [list(map(int, input().split())) for _ in range(n)]
+
+chicken_xy = []
+home_xy = []
+for i in range(n) :
+    for j in range(n) :
+        if map_[i][j] == 2 :
+            chicken_xy.append((i,j))
+        elif map_[i][j] == 1:
+            home_xy.append((i,j))
+print(chicken_xy)
+combination = []
+visit = [False for _ in range(len(chicken_xy))]
+
+make_combination(0, [], visit, 0)
+
+print(combination)
+print(len(combination))
+
+dx, dy = [-1, 0, 1, 1, 1, 0, -1, -1], [-1, -1, -1, 0, 1, 1, 1, 0]
+ans = 9999999
+for chicken_ in combination :
+    sum_ = 0
+    for home_ in home_xy :
+        min_ = 99999
+        for tuple_ in chicken_ :
+            minus = abs(tuple_[0] - home_[0]) + abs(tuple_[1] - home_[1])
+            if min_ > minus :
+                min_ = minus
+        sum_ += min_
+    if sum_ < ans :
+        ans = sum_
+print(ans)
