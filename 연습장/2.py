c5f42dc3adf07a884e4ab645a2d011b59d5aa718
totwjfakd <totwjfakd@gmail.com>
2024-03-09 00:56:38 +0900

    2024.03.09

--- 연습장/2.py
+++ 연습장/2.py
@@ -1,5 +1,4 @@
 import sys
-
 input = sys.stdin.readline
 
 def find_root(x) :
@@ -7,30 +6,42 @@ def find_root(x) :
         parent[x] = find_root(parent[x])
     return parent[x]
 
-def union(a, b) :
+def union(a,b) :
     a = find_root(a)
     b = find_root(b)
 
-    if a<b :
+    if a < b :
         parent[b] = a
     else :
         parent[a] = b
 
-n = int(input())
-m = int(input())
+n, m = map(int, input().split())
+parent = [i for i in range(n+1)]
+know = list(map(int, input().split()))
+party = [list(map(int, input().split())) for _ in range(m)]
 
-map_ = [list(map(int, input().split())) for _ in range(n)]
-parent = [i for i in range(n)]
-seq = list(map(int, input().split()))
-seq = list(map(lambda x:x-1, seq))
-
-for i in range(n) :
-    for j in range(i+1,n) :
-        if map_[i][j] == 1 :
-            union(i, j)
-for i in range(m-1) :
-    if find_root(seq[i]) != find_root(seq[i+1]) :
-        print("NO")
-        break
+if know[0] == 0 :
+    print(m)
 else :
-    print("YES")
+    count = 0
+    if know[0] == 1 :
+        root = know[1]
+    else :
+        for i in range(1, know[0]) :
+            union(know[i], know[i+1])
+        root = parent[1]
+
+    for i in range(m):
+        if len(party[i]) > 2 :
+            u = party[i][1:]
+            for j in range(len(u)-1) :
+                union(u[j], u[j+1])
+        
+    for p in party :
+        if find_root(p[1]) != find_root(root) :
+            count += 1
+    print(count)
+
+
+
+
